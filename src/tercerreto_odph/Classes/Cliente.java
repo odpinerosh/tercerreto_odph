698468f02acebdfe326d94f57b2507c6873e6c8e
Daniel Piñeros <odpinerosh@gmail.com>
2021-08-29 08:24:16 -0500

    Commit Inicial

---
+++ src/tercerreto_odph/Classes/Cliente.java
@@ -0,0 +1,54 @@
+package tercerreto_odph.Classes;
+
+/**
+ * @author odpinerosh
+ */
+
+public class Cliente {
+    private String docCliente;
+    private String nomCliente;
+    private String emailCliente;
+    private String celCliente;
+    private String dirCliente;
+    
+    public void setDocCliente (String auxi) {
+        this.docCliente = auxi;
+    }
+    
+    public String getDocCliente () {
+        return this.docCliente;
+    }
+    
+    public void setNomCliente (String auxi) {
+        this.nomCliente = auxi;
+    }
+    
+    public String getNomCliente () {
+        return this.nomCliente;
+    }
+    
+    public void setEmailCliente (String auxi) {
+        this.emailCliente = auxi;
+    }
+    
+    public String getEmailCliente () {
+        return this.emailCliente;
+    }
+    
+    public void setCelCliente (String auxi) {
+        this.celCliente = auxi;
+    }
+    
+    public String getCelCliente () {
+        return this.celCliente;
+    }
+    
+    public void setDirCliente (String auxi) {
+        this.dirCliente = auxi;
+    }
+    
+    public String getDirCliente () {
+        return dirCliente;
+    }
+    
+}
