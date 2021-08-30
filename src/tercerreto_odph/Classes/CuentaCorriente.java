a023a25fbd706be0db568fd6f51ce919aabf41d1
Daniel Piñeros <odpinerosh@gmail.com>
2021-08-30 06:49:10 -0500

    feat(add) crear clase CuentaCorriente

---
+++ src/tercerreto_odph/Classes/CuentaCorriente.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tercerreto_odph.Classes;
+
+/**
+ *
+ * @author odpinerosh
+ */
+public class CuentaCorriente extends Cuenta {
+
+    @Override
+    public double calcInteres() {
+        double interesCuenta = 0;
+        
+        
+        
+        return interesCuenta;
+    }
+}
